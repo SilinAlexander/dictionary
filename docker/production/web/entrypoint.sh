f6678ce2ec9340322b580100a1a12a087f771c7b
SilinAlexander <asilin1997@mail.ru>
2020-12-15 21:11:43 +0500

    https.prod upd3

--- docker/production/web/entrypoint.sh
+++ docker/production/web/entrypoint.sh
@@ -4,4 +4,5 @@ ls
 python manage.py makemigrations
 python manage.py migrate
 #python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic --no-input
 gunicorn task.wsgi:application --bind 0.0.0.0:8000
